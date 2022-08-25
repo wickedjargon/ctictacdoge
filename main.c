a22125a14ae03462fd08ed9f3c5d1551e13fa07b
Farzin Firouzi <farzinf@riseup.net>
2022-08-24 22:08:34 -0400

    --

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include "DogeLib/Includes.h"
+#include "DogeLib/Text.h"
 #include <stdio.h>
 
 int main(void) {
-  printf("hello world\n");
+
+  int w = 4;
+  int* x = &w;
+  *x = 10;
+  printf("%p \n%i\n", (void *) x, *x);
+  printf("%i\n", w);
   init();
   while (1) {
     const uint T = frameStart();
+    setTextSize(16);
+    setTextColor(WHITE);
+    drawTextCenteredCoord("hello world", getWindowMid());
     frameEnd(T);
   }
   return 0;
