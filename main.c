1cc3571494859f6e5e0dd7c0a2b48234da9c0e8d
Farzin Firouzi <farzineff@gmail.com>
2022-08-25 00:52:48 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -12,10 +12,9 @@ void draw_board(Marker board[3][3]) {
     setTextSize(length/3);
     const uint thickness = 8;
     const Coord top_left = coordOffset(getWindowMid(), iC(-length/2, -length/2));
-    const Coord bottom_right = coordOffset(top_left, iC(length, length));
     setColor(WHITE);
-    for (uint i = 0; i < 3; i++) {
-        const Coord top = coordShift(top_left, DIR_R, length/3);
+    for (uint i = 1; i < 3; i++) {
+        const Coord top = coordShift(top_left, DIR_R, (length/3)*i);
         const Coord left = coordShift(top_left, DIR_D, (length/3)*i);
         fillRectCoordLength(top, iC(thickness, length));
         fillRectCoordLength(left, iC(length, thickness));
@@ -23,28 +22,31 @@ void draw_board(Marker board[3][3]) {
     for (uint x = 0; x < 3; x++) {
         for (uint y = 0; y < 3; y++) {
             if (board[x][y] == M_X) {
-                setColor(RED);
+                setTextColor(RED);
+                const Offset off = iC((length/3)*x, (length/3)*y);
+
+                drawTextCoord("X", coordOffset(top_left, off));
+
             } else if (board[x][y] == M_X) {
-                setColor(BLUE);
+                setTextColor(BLUE);
             }
         }
     }
 }
 
 int main(void) {
-    int w = 4;
-    int* x = &w;
-    *x = 10;
     Marker board [3][3] = {0};
-    printf("%p \n%i\n", (void *) x, *x);
-    printf("%i\n", w);
+    board[0][0] = M_X;
+    board[1][1] = M_X;
+    board[2][2] = M_X;
+
     init();
     while (1) {
         const uint T = frameStart();
+
         draw_board(board);
         setTextSize(16);
         setTextColor(WHITE);
-        drawTextCenteredCoord("hello world", getWindowMid());
         frameEnd(T);
     }
     return 0;
