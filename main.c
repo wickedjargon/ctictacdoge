1a6860aa6443227a004965b30c38ee63ba613f7a
Farzin Firouzi <farzineff@gmail.com>
2022-09-14 18:32:01 -0400

    Automatic commit.

--- main.c
+++ main.c
@@ -39,12 +39,46 @@ void draw_board(Marker board[3][3]) {
 void place_random_marker(Marker board[3][3], Marker marker) {
     int random_x;
     int random_y;
-
     do {
         random_x = rand() % 3;
         random_y = rand() % 3;
     } while (board[random_x][random_y] != M_EMPTY);
     board[random_x][random_y] = marker;
+}
+
+// TODO: `win_check` function: game over function
+// x wins, y wins, tie
+// M_X, M_O, M_EMPTY
+
+bool is_full(Marker board[3][3]) {
+    for (uint x = 0; x < 3; x++) {
+        for (uint y = 0; y < 3; y++) {
+            if (board[x][y] == M_EMPTY) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool horzont_won(Makrer board[3][3], Marker player) {
+    for (y = 0; y < 3; y++) {
+        for (x = 0; x < 3; x++) {
+            if (board[x][y] == player && board[x][y] == player
+        }
+    }
+
+}
+
+bool player_win(Marker board[3][3], Marker player) {
+    if (vert_win(board, player)) {
+        return true;
+    } else if (horizont_won(board, player)) {
+        return true;
+    } else if (diag_won(board, player)) {
+        return true;
+    }
+
 }
 
 int main(void) {
@@ -55,7 +89,6 @@ int main(void) {
     /* board[0][1] = M_O; */
     Marker whose_turn = M_X;
     init();
-
     place_random_marker(board, M_X);
     place_random_marker(board, M_O);
     place_random_marker(board, M_X);
@@ -63,10 +96,10 @@ int main(void) {
         const uint T = frameStart();
         if (keyPressed(SDL_SCANCODE_Q) || keyPressed(SDL_SCANCODE_ESCAPE)) {
             exit(0);
-            }
+        }
 
         draw_board(board);
-        frameEnd(T);
+        frameEnd(T)
     }
     return 0;
 }
